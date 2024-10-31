972315382cca34422347c2054e16516aef022e5e
Landon Osteen <landonwyatteosteen@gmail.com>
2024-10-31 16:17:44 -0400

    HFT update

--- config.js
+++ config.js
@@ -16,26 +16,37 @@ module.exports = {
     apiKey: process.env.POLYGON_API_KEY,
   },
   orderSettings: {
-    limitOffsetCents: 3,
+    limitOffsetCents: 2,
     profitTargets: [
+      { targetCents: 1, percentToClose: 30 },
+      { targetCents: 2, percentToClose: 30 },
+      { targetCents: 3, percentToClose: 30 },
+      { targetCents: 4, percentToClose: 30 },
+      { targetCents: 5, percentToClose: 30 },
+      { targetCents: 6, percentToClose: 30 },
+      { targetCents: 7, percentToClose: 30 },
       { targetCents: 8, percentToClose: 30 },
-      { targetCents: 10, percentToClose: 70 },
-      { targetCents: 15, percentToClose: 50 },
-      { targetCents: 20, percentToClose: 100 },
+      { targetCents: 9, percentToClose: 30 },
+      { targetCents: 10, percentToClose: 100 },
     ],
     dynamicStops: [
-      { profitTargetsHit: 0, stopCents: -25 },
-      { profitTargetsHit: 1, stopCents: -10 },
-      { profitTargetsHit: 2, stopCents: 0 },
+      { profitTargetsHit: 0, stopCents: -5 },
+      { profitTargetsHit: 1, stopCents: 0 },
+      { profitTargetsHit: 2, stopCents: 1 },
+      { profitTargetsHit: 3, stopCents: 2 },
+      { profitTargetsHit: 4, stopCents: 3 },
+      { profitTargetsHit: 5, stopCents: 4 },
+      { profitTargetsHit: 6, stopCents: 5 },
+      { profitTargetsHit: 7, stopCents: 6 },
+      { profitTargetsHit: 8, stopCents: 7 },
+      { profitTargetsHit: 9, stopCents: 8 },
+      { profitTargetsHit: 10, stopCents: 9 },
     ],
-    pyramidLevels: [
-      { addInCents: 2, percentToAdd: 200, offsetCents: 3 },
-      { addInCents: 12, percentToAdd: 30, offsetCents: 3 },
-    ],
+    pyramidLevels: [{ addInCents: 15, percentToAdd: 20, offsetCents: 3 }],
   },
   pollingIntervals: {
     orderStatus: 1000, // Poll order statuses every second
-    positionRefresh: 2000, // Refresh positions every 2 seconds
+    positionRefresh: 1500, // Refresh positions every 2 seconds
   },
   logging: {
     level: 'info', // Logging level: 'debug', 'info', 'warn', 'error'
