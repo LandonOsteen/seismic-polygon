d8872d2f78bf98d7f2ca520ad03f6871685730ac
Landon Osteen <landonwyatteosteen@gmail.com>
2024-11-02 14:28:13 -0400

    scalping setup

--- config.js
+++ config.js
@@ -20,66 +20,37 @@ module.exports = {
   orderSettingsUnder10: {
     limitOffsetCents: 2,
     profitTargets: [
-      { targetCents: 5, percentToClose: 20 }, // Target 1
-      { targetCents: 10, percentToClose: 30 }, // Target 2
-      { targetCents: 15, percentToClose: 20 }, // Target 3
-      { targetCents: 20, percentToClose: 30 }, // Target 4
-      { targetCents: 30, percentToClose: 20 }, // Target 5
-      { targetCents: 40, percentToClose: 20 }, // Target 6
-      { targetCents: 50, percentToClose: 50 }, // Target 7
-      { targetCents: 60, percentToClose: 20 }, // Target 8
-      { targetCents: 70, percentToClose: 20 }, // Target 9
-      { targetCents: 80, percentToClose: 20 }, // Target 10
-      { targetCents: 90, percentToClose: 20 }, // Target 11
-      { targetCents: 100, percentToClose: 100 }, // Target 12
+      { targetCents: 4, percentToClose: 40 }, // Target 1
+      { targetCents: 8, percentToClose: 70 }, // Target 2
+      { targetCents: 15, percentToClose: 100 }, // Target 3
     ],
     dynamicStops: [
       { profitTargetsHit: 0, stopCents: -20 },
-      { profitTargetsHit: 1, stopCents: -15 },
-      { profitTargetsHit: 2, stopCents: -10 },
-      { profitTargetsHit: 3, stopCents: 5 },
-      { profitTargetsHit: 4, stopCents: 10 },
-      { profitTargetsHit: 5, stopCents: 15 },
-      { profitTargetsHit: 6, stopCents: 20 },
-      { profitTargetsHit: 7, stopCents: 25 },
-      { profitTargetsHit: 8, stopCents: 40 },
-      { profitTargetsHit: 9, stopCents: 50 },
-      { profitTargetsHit: 10, stopCents: 60 },
-      { profitTargetsHit: 11, stopCents: 70 },
+      { profitTargetsHit: 1, stopCents: -10 },
+      { profitTargetsHit: 2, stopCents: 0 },
     ],
-    pyramidLevels: [{ addInCents: 10, percentToAdd: 30, offsetCents: 3 }],
+    pyramidLevels: [
+      { addInCents: 5, percentToAdd: 100, offsetCents: 3 },
+      { addInCents: 10, percentToAdd: 50, offsetCents: 3 },
+    ],
   },
 
   orderSettingsOver10: {
-    limitOffsetCents: 3,
+    limitOffsetCents: 2,
     profitTargets: [
-      { targetCents: 6, percentToClose: 20 }, // Target 1
-      { targetCents: 12, percentToClose: 30 }, // Target 2
-      { targetCents: 18, percentToClose: 20 }, // Target 3
-      { targetCents: 24, percentToClose: 30 }, // Target 4
-      { targetCents: 36, percentToClose: 20 }, // Target 5
-      { targetCents: 48, percentToClose: 20 }, // Target 6
-      { targetCents: 60, percentToClose: 50 }, // Target 7
-      { targetCents: 72, percentToClose: 20 }, // Target 8
-      { targetCents: 84, percentToClose: 20 }, // Target 9
-      { targetCents: 96, percentToClose: 20 }, // Target 10
-      { targetCents: 108, percentToClose: 20 }, // Target 11
-      { targetCents: 120, percentToClose: 100 }, // Target 12
+      { targetCents: 4, percentToClose: 40 }, // Target 1
+      { targetCents: 8, percentToClose: 70 }, // Target 2
+      { targetCents: 15, percentToClose: 100 }, // Target 3
     ],
     dynamicStops: [
       { profitTargetsHit: 0, stopCents: -25 },
-      { profitTargetsHit: 2, stopCents: -15 },
-      { profitTargetsHit: 3, stopCents: 5 },
-      { profitTargetsHit: 4, stopCents: 10 },
-      { profitTargetsHit: 5, stopCents: 15 },
-      { profitTargetsHit: 6, stopCents: 20 },
-      { profitTargetsHit: 7, stopCents: 30 },
-      { profitTargetsHit: 8, stopCents: 40 },
-      { profitTargetsHit: 9, stopCents: 50 },
-      { profitTargetsHit: 10, stopCents: 60 },
-      { profitTargetsHit: 11, stopCents: 70 },
+      { profitTargetsHit: 1, stopCents: -10 },
+      { profitTargetsHit: 2, stopCents: 0 },
     ],
-    pyramidLevels: [{ addInCents: 12, percentToAdd: 30, offsetCents: 4 }],
+    pyramidLevels: [
+      { addInCents: 5, percentToAdd: 100, offsetCents: 3 },
+      { addInCents: 10, percentToAdd: 50, offsetCents: 3 },
+    ],
   },
 
   pollingIntervals: {
